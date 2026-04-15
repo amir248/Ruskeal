aa41f65d90aa48652a857564ec9802d2e6a8992e
Amir <chikchicly@gmail.com>
2026-04-15 15:14:35 +0700

    photo

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,34 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const lightbox = document.getElementById("lightbox");
+
+  if (!lightbox) return;
+
+  const images = document.querySelectorAll(".gallery-item img");
   const lightboxImg = document.querySelector(".lightbox-img");
   const closeBtn = document.querySelector(".lightbox-close");
 
-  document.querySelectorAll(".gallery-item img").forEach(img => {
+  images.forEach(img => {
     img.addEventListener("click", () => {
       lightbox.style.display = "flex";
       lightboxImg.src = img.src;
     });
   });
 
-  closeBtn.addEventListener("click", () => {
-    lightbox.style.display = "none";
-  });
+  if (closeBtn) {
+    closeBtn.onclick = () => lightbox.style.display = "none";
+  }
 
-  lightbox.addEventListener("click", (e) => {
+  lightbox.onclick = (e) => {
     if (e.target === lightbox) {
       lightbox.style.display = "none";
     }
-  });
+  };
 
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      lightbox.style.display = "none";
-    }
-  });
-
-});
-console.log("JS работает");
-window.addEventListener("click", () => {
-  console.log("клик работает");
 });
